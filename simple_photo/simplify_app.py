5c0508ff02b0a89c283a0590e75ecde521052303
MINUS-1ONE <644156461@qq.com>
2019-09-19 00:28:39 +0800

    添加message窗口发出警告 完善了下一张图片的按钮 并在Label上加上检测鼠标 最后添加了画框函数 下一步加上listbox

--- simple_photo/simplify_app.py
+++ simple_photo/simplify_app.py
@@ -1,5 +1,6 @@
 from glob import glob
 import argparse
+import tkinter.messagebox as msgbox
 from PIL import Image, ImageTk
 import tkinter as tki
 import scipy.misc as sm
@@ -16,6 +17,8 @@ class PhotoBooth(tki.Frame):
 		self.createWeight()
 		
 		self.sample = []
+		self.panelLabel = None
+		self.listpos = 0
 		
 	def createWeight(self):
 		# 打开图片：直接打开图片和打开文件夹目录
@@ -48,39 +51,122 @@ class PhotoBooth(tki.Frame):
 		pass
 		
 	def opendir(self):
-		dirpath = self.dirPathEntry.get()
-		file_name=glob(dirpath+"/*png")
-		
-		for file in file_name:
-			pic = sm.imread(file).astype(np.float32)
-			pic = sm.imresize(pic, (400,300)).astype(np.float32)
-			self.sample.append(pic)
-		 
-		self.sample = np.array(self.sample)
-		
-		# 显示第一张图片
-		pic = self.sample[0,:,:,:]
-		pic = np.reshape(pic,(400,300,3)).astype(np.uint8)
-		image = Image.fromarray(pic)
-		image = ImageTk.PhotoImage(image)
-		self.panelLabel.configure(image=image)
-		self.panelLabel.image = image
+		self.sample = []
+		if self.dirPathEntry.get() != None:
+			self.listpos = 0
+			dirpath = self.dirPathEntry.get()
+			file_name=glob(dirpath+"/*png")
+			
+			for file in file_name:
+				pic = sm.imread(file).astype(np.float32)
+				#pic = sm.imresize(pic, (400,300)).astype(np.float32)
+				self.sample.append(pic)
+			 
+			self.sample = np.array(self.sample)
+			# 显示第一张图片
+			pic = self.sample[self.listpos]
+			#pic = np.reshape(pic,(400,300,3)).astype(np.uint8)
+			pic = pic.astype(np.uint8)
+			image = Image.fromarray(pic)
+			image = ImageTk.PhotoImage(image)
+			if self.panelLabel is None:
+				self.panelLabel = tki.Label(self, image=image)
+				self.panelLabel.image = image
+				self.panelLabel.bind("<Button-1>",self.rtastart)
+				self.panelLabel.bind("<ButtonRelease-1>",self.rtaend)
+				self.panelLabel.grid(row=0, column=2, rowspan=4, columnspan=4,
+					sticky=tki.W+tki.E+tki.N+tki.S, padx=10, pady=10)
+				
+			else:
+				self.panelLabel.configure(image=image)
+				self.panelLabel.image = image
+			
+		else:
+			msgbox.showwarning(title="[WARNING]",message="THE DIR PATH IS NOT FILLED IN!")
 		
 	def lastimg(self):
-		pass
+		if self.listpos > 0:
+			self.listpos -= 1
+			pic = self.sample[self.listpos]
+			pic = pic.astype(np.uint8)
+			image = Image.fromarray(pic)
+			image = ImageTk.PhotoImage(image)
+			if self.panelLabel != None:
+				self.panelLabel.configure(image=image)
+				self.panelLabel.image = image
+			else:
+				msgbox.showwarning(title="[WARNING]",message="NO RESOURSE!")
+		else:
+			msgbox.showwarning(title="[WARNING]",message="THIS IS THE FIRST FRAME!")
 		
 	def nextimg(self):
-		pic = self.sample[1,:,:,:]
-		pic = pic.astype(np.uint8)
-		image = Image.fromarray(pic)
-		image = ImageTk.PhotoImage(image)
-		self.panelLabel.configure(image=image)
-		self.panelLabel.image = image
+		the_last_frame = self.sample.shape[0]
+		if self.listpos < the_last_frame:
+			self.listpos += 1
+			pic = self.sample[self.listpos]
+			pic = pic.astype(np.uint8)
+			image = Image.fromarray(pic)
+			image = ImageTk.PhotoImage(image)
+			if self.panelLabel != None:
+				self.panelLabel.configure(image=image)
+				self.panelLabel.image = image
+			else:
+				msgbox.showwarning(title="[WARNING]",message="NO RESOURSE!")
+		else:
+			msgbox.showwarning(title="[WARNING]",message="THIS IS THE LAST FRAME!")				
 		
 	def save(self):
 		pass
 		
-		
+	def rtaswitch(self,event):
+		# 画框操作的开关 检测listbox变量的listvariable文本内容（self.statuslist.get()）
+		# 如果listbox被按下 则listvariable内容（self.statuslist.get()）增加了listbox的的value 开关打开
+		if self.statuslist.get() != None:
+			self.switch_rta = True
+			self.status=self.fm5_middle.statebox.get(int(self.fm5_middle.statebox.curselection()[0]))
+			
+		else:
+			self.switch_rta = False
+	
+	def rtastart(self,event):
+		# 读取鼠标落点
+		self.startx = event.x
+		self.starty = event.y
+	
+	def rtaend(self,event):
+		if self.switch_rta == True:
+			# 读取鼠标释放点
+			self.endx = event.x
+			self.endy = event.y
+			# 从Image格式转化为OpenCV格式
+			img_rect = cv2.cvtColor(np.asarray(self._PILImage[self.listpos]),
+				cv2.COLOR_RGB2BGR)
+			# 进行画矩形框操作
+			print("[INFO] Start drawing frame")
+			cv2.rectangle(img=img_rect,pt1=(self.startx,self.starty),
+				pt2=(self.endx,self.endy),color=(0,255,0),thickness=1)
+			# 从OpenCV格式转化为Image格式
+			# 画框操作保留副本 即可保留多个框
+			self._PILImage[self.listpos] = Image.fromarray(cv2.cvtColor(img_rect,cv2.COLOR_BGR2RGB))
+			self.imagelist[self.listpos] = Image.fromarray(cv2.cvtColor(img_rect,cv2.COLOR_BGR2RGB))
+			# 重新化为ImageTk格式
+			self.imagelist[self.listpos] = ImageTk.PhotoImage(self.imagelist[self.listpos])
+			self.FrameLabel.configure(image=self.imagelist[self.listpos])
+			self.FrameLabel.image = self.imagelist[self.listpos]
+			print("[INFO] finish drawing frame")
+			print("[INFO] Write the information for the frame")
+			with open("Frame_infor.txt",'a') as f_msg:
+				f_msg.write("{0}，{1}，{2}，{3}，{4}第{5}帧\n".format(
+					self.status,
+					(self.startx,self.starty),
+					self.endx-self.startx,
+					self.endy-self.starty,
+					self.VideoFromEntry.get(),
+					self.listpos)
+					)
+		else:
+			msgbox.showwarning(title="[WARNING]",message="CATEGORY NOT SELECTED!")
+				
 		
 		
 		
